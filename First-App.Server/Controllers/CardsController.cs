f20e395a2573789c312d46d31d74458785446180
Vladyslav <vladmelhnik@gmail.com>
2024-05-04 00:54:50 +0300

    init api endpoints

---
+++ First-App.Server/Controllers/CardsController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace First_App.Server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CardsController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> AddCard()
+        {
+            return Ok();
+        }
+        [HttpGet]
+        public async Task<IActionResult> GetCards()
+        {
+            return Ok();
+        }
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetCard(int id)
+        {
+            return Ok();
+        }
+        [HttpPatch("{id}")]
+        public async Task<IActionResult> EditCard(int id)
+        {
+            return Ok();
+        }
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteCard(int id)
+        {
+            return Ok();
+        }
+    }
+}
