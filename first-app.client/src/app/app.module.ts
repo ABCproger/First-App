d10dd03c395b96678128af5abbe4b02caaab93fc
Vladyslav <vladmelhnik@gmail.com>
2024-05-08 22:30:08 +0300

    Generate card-component, card-list, new-list with Angular CLI

--- first-app.client/src/app/app.module.ts
+++ first-app.client/src/app/app.module.ts
@@ -4,10 +4,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CardComponentComponent } from './card-component/card-component.component';
+import { CardListComponent } from './card-list/card-list.component';
+import { NewListComponent } from './new-list/new-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CardComponentComponent,
+    CardListComponent,
+    NewListComponent
   ],
   imports: [
     BrowserModule, HttpClientModule,
---
+++ first-app.client/src/app/card-component/card-component.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-card-component',
+  templateUrl: './card-component.component.html',
+  styleUrl: './card-component.component.css'
+})
+export class CardComponentComponent {
+
+}
